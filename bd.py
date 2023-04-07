fda0563abcdc9428cbb03aca8ebbc52d32115043
dev-rafa1707 <rafa1707@gmail.com>
2023-04-07 07:15:58 -0300

    add bd.py como banco de dados teste local

---
+++ bd.py
@@ -0,0 +1,20 @@
+carros = [
+    {
+    'id':'1',
+    'marca':'Jeep',
+    'modelo':'Compass',
+    'ano':2017
+    },
+    {
+    'id':'2',
+    'marca':'Mitsubisho',
+    'modelo':'ASX 4x4',
+    'ano':2015
+    },
+    {
+    'id':'3',
+    'marca':'Toyota',
+    'modelo':'Yaris',
+    'ano':2022
+    },
+]
