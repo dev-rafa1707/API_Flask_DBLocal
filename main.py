cff0a09028e665c18e2b82a1ddaa0a15fad0b791
dev-rafa1707 <rafa1707@gmail.com>
2023-04-07 08:57:30 -0300

    create message/data standar for response

--- main.py
+++ main.py
@@ -3,19 +3,25 @@ from bd import Cars
 
 
 app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
 
 
 @app.route('/cars', methods=['GET'])
 def get_cars():
     return make_response(
-        jsonify(Cars)
+        jsonify(message='Cars List',
+                data=Cars)
     )
 
 @app.route('/cars', methods=['POST'])
 def create_car():
     car = request.json
     Cars.append(car)
-    return car
+    return make_response(
+        jsonify(message='Successful registration',
+                data=car
+        )
+    ) 
 
 
 app.run()
